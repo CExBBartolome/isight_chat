7b6a478756d88c4266f755ba792621754df79c60
CExBBartolome <bbartolome@i-sight.com>
2014-12-02 16:59:36 -0500

    Separated tests for app.js and lib/server.js

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,29 +1,11 @@
 "use strict";
 var mocha = require('mocha');
 var assert = require('assert');
-var server;
-
-suite("lib/server.js",function(){
-	test("it should throw an exception if no port was provided", function(){
-		assert.throws(function(){
-			require('../../lib/server.js')();
-		});
-	})
-	test("it should throw an exception if port is invalid", function(){
-		assert.throws(function(){
-			require('../../lib/server.js')('string');
-		})
-	});
-	
-	test("is not undefined",function(){
-		server = require('../../lib/server.js')(1337);
-		assert.notEqual(typeof server,'undefined');
-	});
-	// test if listening on port 1337
-	// might need to use proxyquire
-});
+var seneca = require('seneca');
 
 // test created microservice is able to receive messages
 suite("app.js",function(){
-
+	test("it should successfully use app.js in seneca",function(){
+		//seneca.use('../../app.js');
+	});
 });
---
+++ test/unit/lib/server.js
@@ -0,0 +1,25 @@
+"use strict";
+var mocha = require('mocha');
+var assert = require('assert');
+var server;
+var seneca = require('seneca');
+
+suite("lib/server.js",function(){
+	test("it should throw an exception if no port was provided", function(){
+		assert.throws(function(){
+			require('../../../lib/server.js')();
+		});
+	})
+	test("it should throw an exception if port is invalid", function(){
+		assert.throws(function(){
+			require('../../../lib/server.js')('string');
+		})
+	});
+	
+	test("is not undefined",function(){
+		server = require('../../../lib/server.js')(1337);
+		assert.notEqual(typeof server,'undefined');
+	});
+	// test if listening on port 1337
+	// might need to use proxyquire
+});
