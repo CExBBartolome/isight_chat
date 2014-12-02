c57d82fb87e7a7fbcb79545355bdfb8aeb4cd96d
CExBBartolome <bbartolome@i-sight.com>
2014-12-02 12:25:47 -0500

    Added new test for creating a server - port number validation - check if expose server() function is not undefined

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,14 +1,26 @@
 "use strict";
 var mocha = require('mocha');
 var assert = require('assert');
-var app = require("../../app.js");
+var app;
 
 suite("app.js",function(){
-	test("#server()",function(){
-		test("it should throw an exception if no parameters are provided",
+	test("#server() is not undefined",function(){
+		app = require('../../app.js');
+		// console.log("typeof:"+(typeof app.server));
+		assert.notEqual(typeof app.server,'undefined');
+	});
+	suite("#server()",function(){
+		test("it should throw an exception if no port was provided", function(){
 			assert.throws(function(){
 				app.server();
+			});
+		})
+	});
+	suite("#server({invalid})",function(){
+		test("it should throw an exception if port is invalid", function(){
+			assert.throws(function(){
+				app.server('string');
 			})
-		);
+		});
 	});
 });
