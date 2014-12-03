5e3c8fca39f32f923cbe91050afbbe5c1ee21b80
CExBBartolome <bbartolome@i-sight.com>
2014-12-03 00:59:15 -0500

    Fixed seneca.use for test/unit/app.js

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,11 +1,14 @@
 "use strict";
 var mocha = require('mocha');
 var assert = require('assert');
-var seneca = require('seneca');
+var seneca = require('seneca')();
 
 // test created microservice is able to receive messages
 suite("app.js",function(){
 	test("it should successfully use app.js in seneca",function(){
-		//seneca.use('../../app.js');
+		var pattern = {cmd:"chat"};
+		var opts = {pattern:pattern,port:1338}
+
+		seneca.use('../../app.js',opts);
 	});
 });
